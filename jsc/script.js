65163b2922e8274a1762d0c1e6a2c955dfcdd0c8
eskander-gtari <58818724+eskander-gtari@users.noreply.github.com>
2020-10-23 11:47:50 +0200

    Add files via upload

---
+++ jsc/script.js
@@ -0,0 +1,13 @@
+a=parseInt(document.fo.tx1.value);
+b=parseInt(document.fo.tx2.value);
+c=parseInt(document.fo.tx3.value);
+d=parseInt(document.fo.tx4.value);
+const inputs = document.querySelectorAll('.teslar');
+
+function f(){          
+for(i=0;i<inputs.length;i++){
+    
+        document.fo.tx.value=inputs[i];
+}
+    
+}
